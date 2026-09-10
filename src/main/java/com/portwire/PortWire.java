37e8d1206a4eaa7e06f5e55756dd899ec758fbba
Arsam <arsam4waffels@gmail.com>
2026-09-10 23:14:00 +0330

    core: initialize Input/Output stream

--- src/main/java/com/portwire/PortWire.java
+++ src/main/java/com/portwire/PortWire.java
@@ -1,6 +1,8 @@
 package com.portwire;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,6 +15,12 @@ public class PortWire {
 
             // accepts the connection with a client
             Socket socket = serverSocket.accept();
+
+            // get client input
+            InputStream inputStream = socket.getInputStream();
+
+            // send client the output
+            OutputStream outputStream = socket.getOutputStream();
 
         } catch (IOException e) {
             throw new IOException(e);
