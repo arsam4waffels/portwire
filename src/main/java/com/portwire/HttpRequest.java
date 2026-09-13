2c9a47a49122e9c26cd60c1bbd69febc7f9c1489
Arsam <arsam4waffels@gmail.com>
2026-09-13 17:17:09 +0330

    docs: add Javadoc to HTTPRequest

--- src/main/java/com/portwire/HttpRequest.java
+++ src/main/java/com/portwire/HttpRequest.java
@@ -3,12 +3,26 @@ package com.portwire;
 import java.util.Map;
 
 public class HttpRequest {
-
+    /**
+     * <h5>Client data retention</h5>
+     * <p>A container is created for all the connection
+     * protocol attributes to store the information sent
+     * from the client.</p>
+     */
     private final String method;
     private final String path;
     private final String version;
     private final Map<String, String> headers;
 
+    /**
+     * <h5>Class constructor</h5>
+     * <p>Four variables are received. These are requests sent
+     * from the client.</p>
+     * @param method Requested method
+     * @param path Requested address
+     * @param version Client version
+     * @param headers Requested topic
+     */
     public HttpRequest(String method,
                        String path,
                        String version,
@@ -19,6 +33,7 @@ public class HttpRequest {
         this.headers = headers;
     }
 
+    // Methods for encapsulation and returning data
     public String getMethod() {
         return method;
     }
@@ -33,6 +48,7 @@ public class HttpRequest {
         return headers.get(name.toLowerCase());
     }
 
+    // Convert to text string
     @Override
     public String toString() {
         return method + " " + path + " " + version;
